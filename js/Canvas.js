34db9d162b2f8c8577014563ce6a502856b14f71
Krishnakumar Muraleedharan <krishnakumarm24386@gmail.com>
2016-06-14 18:13:53 +0530

    Update Canvas.js

--- js/Canvas.js
+++ js/Canvas.js
@@ -42,3 +42,36 @@ Canvas.prototype.text = function(txt,x,y,style,font) {
 	this.ctx.font=font;
 	this.ctx.fillText(txt,x,y);
 };
+
+Canvas.prototype.roundRect = function(x, y, width, height, radius, fill, stroke) {
+
+  if (typeof radius === 'undefined') {
+    radius = 5;
+  }
+  if (typeof radius === 'number') {
+    radius = {tl: radius, tr: radius, br: radius, bl: radius};
+  } else {
+    var defaultRadius = {tl: 0, tr: 0, br: 0, bl: 0};
+    for (var side in defaultRadius) {
+      radius[side] = radius[side] || defaultRadius[side];
+    }
+  }
+  this.ctx.beginPath();
+  this.ctx.moveTo(x + radius.tl, y);
+  this.ctx.lineTo(x + width - radius.tr, y);
+  this.ctx.quadraticCurveTo(x + width, y, x + width, y + radius.tr);
+  this.ctx.lineTo(x + width, y + height - radius.br);
+  this.ctx.quadraticCurveTo(x + width, y + height, x + width - radius.br, y + height);
+  this.ctx.lineTo(x + radius.bl, y + height);
+  this.ctx.quadraticCurveTo(x, y + height, x, y + height - radius.bl);
+  this.ctx.lineTo(x, y + radius.tl);
+  this.ctx.quadraticCurveTo(x, y, x + radius.tl, y);
+  this.ctx.closePath();
+  if (fill) {
+    this.ctx.fill();
+  }
+  if (stroke) {
+    this.ctx.stroke();
+  }
+
+};
